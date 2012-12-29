c725cea824525dfcc8333265514c35517af9ea60
Grzegorz Olędzki <grzegon@poczta.onet.pl>
2012-12-29 23:28:29 +0100

    Wydzielenie metody scrapuj()

--- medicover.py
+++ medicover.py
@@ -20,104 +20,76 @@ from scraper import *
 
 class ScraperMedicover(ScraperLekarzy):
   def __init__(self):
+    ScraperLekarzy.__init__(self, adresStartowy="https://online.medicover.pl/WAB3/")
+  def odwiedzIZbierzWyniki(self, sel):
     dzien = 864000000000
     komponentZData = "//input[@name='dtpStartDateTicks']"
 
-    adres="https://online.medicover.pl/WAB3/"
+    sel.wait_for_page_to_load(10000)  
 
-    selenium_conf = slownik['selenium']
-    sel = selenium(selenium_conf["host"], selenium_conf["port"],
-          selenium_conf.get('browser', '*firefox /usr/bin/firefox'), adres)
-    try:
-      sel.start()
-      sel.open(adres)
+    sel.type('id=txUserName', slownik["login"])
+    sel.type('id=txPassword', hasla.haslo('medicover', slownik['login'], slownik.get('haslo')))
+    sel.click('id=btnLogin')
+    sel.wait_for_page_to_load(20000)
 
-      sel.wait_for_page_to_load(10000)  
+    sel.click('id=btnNext')
+    sel.wait_for_page_to_load(10000)
 
-      sel.type('id=txUserName', slownik["login"])
-      sel.type('id=txPassword', hasla.haslo('medicover', slownik['login'], slownik.get('haslo')))
-      sel.click('id=btnLogin')
-      sel.wait_for_page_to_load(20000)
+    sel.click('id=btnBookAppointment')
+    sel.wait_for_page_to_load(10000)
 
-      sel.click('id=btnNext')
-      sel.wait_for_page_to_load(10000)
+    sel.select('id=cboRegion', 'Warszawa')
+    time.sleep(2)
+    
+    if specjalizacja:
+     self.wybierz(sel, 'id=cboSpecialty', specjalizacja) 
+    if sel.is_element_present('id=chkFollowUpVisit'):	# pojawia się po wyborze "Pediatra"
+     sel.click('id=chkFollowUpVisit')
+     time.sleep(2)
+    if doktor:
+     self.wybierz(sel, 'id=cboDoctor', doktor) 
+    if centrum:
+     self.wybierz(sel, 'id=cboClinic', centrum)
 
-      sel.click('id=btnBookAppointment')
-      sel.wait_for_page_to_load(10000)
+    wynik={}
+    
+    dzis = datetime.datetime.now()
+    poczatkowaWartoscPoczatku = int(sel.get_value(komponentZData))
+    max_dni = (przed-dzis).days + 1
 
-      sel.select('id=cboRegion', 'Warszawa')
-      time.sleep(2)
-      
-      if specjalizacja:
-       self.wybierz(sel, 'id=cboSpecialty', specjalizacja) 
-      if sel.is_element_present('id=chkFollowUpVisit'):	# pojawia się po wyborze "Pediatra"
-       sel.click('id=chkFollowUpVisit')
-       time.sleep(2)
-      if doktor:
-       self.wybierz(sel, 'id=cboDoctor', doktor) 
-      if centrum:
-       self.wybierz(sel, 'id=cboClinic', centrum)
+    sel.click('id=btnSearch')
+    sel.wait_for_page_to_load(10000)
+    time.sleep(3)
+    while sel.is_element_present('id=lblLoading'):
+        time.sleep(3)
 
-      wynik={}
-      
-      dzis = datetime.datetime.now()
-      poczatkowaWartoscPoczatku = int(sel.get_value(komponentZData))
-      max_dni = (przed-dzis).days + 1
-
-      sel.click('id=btnSearch')
-      sel.wait_for_page_to_load(10000)
-      time.sleep(3)
-      while sel.is_element_present('id=lblLoading'):
-          time.sleep(3)
-
-      while sel.is_element_present('id=dgGrid'):  
-         dataNapis = sel.get_table('dgGrid.0.0').strip().split(" ")[1].__str__()
-         data = datetime.datetime.strptime(dataNapis, "%d/%m/%Y")
+    while sel.is_element_present('id=dgGrid'):  
+       dataNapis = sel.get_table('dgGrid.0.0').strip().split(" ")[1].__str__()
+       data = datetime.datetime.strptime(dataNapis, "%d/%m/%Y")
+       
+       zaIleDni = (data-dzis).days
+       if data > przed:
+         print "Wybieglismy juz %d dni w przyszlosc, konczymy" % zaIleDni
+         break
          
-         zaIleDni = (data-dzis).days
-         if data > przed:
-           print "Wybieglismy juz %d dni w przyszlosc, konczymy" % zaIleDni
-           break
-           
-         wynikiTegoDnia=[]  
-         wynik[data.strftime("%Y-%m-%d (%A)")] = wynikiTegoDnia  
+       wynikiTegoDnia=[]  
+       wynik[data.strftime("%Y-%m-%d (%A)")] = wynikiTegoDnia  
 
-         ileWierszy = int(sel.get_xpath_count("//*[@id='dgGrid']/*/*"))
-         for wiersz in range(ileWierszy):
-            komorki = [self.pozbadzSiePolskichLiter(sel.get_table('dgGrid.%d.%d' % (wiersz,kolumna))) for kolumna in [1,2,3]]
-            print data, komorki
-            wynikiTegoDnia.append(" ".join(komorki))
-            
-         nextDay="xpath=//input[@id='btnNextDay'][not(@disabled)]" 
-         if sel.is_element_present(nextDay):
-             sel.click(nextDay)
-             sel.wait_for_page_to_load(10000)
-             time.sleep(1)
-         else:
-             break          
+       ileWierszy = int(sel.get_xpath_count("//*[@id='dgGrid']/*/*"))
+       for wiersz in range(ileWierszy):
+          komorki = [self.pozbadzSiePolskichLiter(sel.get_table('dgGrid.%d.%d' % (wiersz,kolumna))) for kolumna in [1,2,3]]
+          print data, komorki
+          wynikiTegoDnia.append(" ".join(komorki))
+          
+       nextDay="xpath=//input[@id='btnNextDay'][not(@disabled)]" 
+       if sel.is_element_present(nextDay):
+           sel.click(nextDay)
+           sel.wait_for_page_to_load(10000)
+           time.sleep(1)
+       else:
+           break          
+    return wynik
 
-      wynikSformatowany=pprint.pformat(sorted(wynik))
-
-      md5 = hashlib.md5()
-      md5.update(wynikSformatowany)
-      skrot = 'pamiec/%s' % (md5.hexdigest())
-      if not wynik or os.path.exists(skrot):
-        print "NIC NOWEGO"
-      else:
-        print "Cos nowego: ", wynik
-        self.mejl(wynik, slownik["email"])
-        plik = open(skrot, 'w')
-        plik.write(wynikSformatowany)
-        plik.close()
-
-    finally:
-       try:
-          sel.close()
-          time.sleep(2)
-          sel.stop()
-          time.sleep(2)
-       except:
-          traceback.print_exc()
 
 if __name__ == "__main__":
   parametry = sys.argv[1:4]
@@ -137,6 +109,6 @@ if __name__ == "__main__":
   print "- doktora       "+doktor
   print "- centrum       "+centrum
   print "Wizyta przed "+str(przed)
-  ScraperMedicover()
+  ScraperMedicover().scrapuj()
   
 
--- scraper.py
+++ scraper.py
@@ -18,6 +18,46 @@ from ustawienia import konta
 import hasla
 
 class ScraperLekarzy: 
+    def __init__(self, adresStartowy):
+      self.adresStartowy = adresStartowy
+
+    def scrapuj(self):
+      try:
+       selenium = self.wystartujSelenium()
+       wynik = self.odwiedzIZbierzWyniki(selenium)
+       self.sprawdzCzyWynikNowyIEwentualnieZakomunikuj(wynik)
+      finally:
+         try: 
+            selenium.close()
+            time.sleep(2)
+            selenium.stop()   
+            time.sleep(2)
+         except:
+            traceback.print_exc()
+
+    def wystartujSelenium(self):
+     selenium_conf = slownik['selenium']
+     sel = selenium(selenium_conf["host"], selenium_conf["port"],
+           selenium_conf.get('browser', '*firefox /usr/bin/firefox'), self.adresStartowy)
+     sel.start()
+     sel.open(self.adresStartowy)
+     return sel
+
+    def sprawdzCzyWynikNowyIEwentualnieZakomunikuj(self, wynik):
+        wynikSformatowany=pprint.pformat(sorted(wynik))
+
+        md5 = hashlib.md5()
+        md5.update(wynikSformatowany)
+        skrot = 'pamiec/%s' % (md5.hexdigest())
+        if not wynik or os.path.exists(skrot): 
+          print "NIC NOWEGO"
+        else:
+          print "Cos nowego: ", wynik
+          self.mejl(wynik, slownik["email"])
+          plik = open(skrot, 'w')
+          plik.write(wynikSformatowany)
+          plik.close()
+
     def pozbadzSiePolskichLiter(self, text):
        dic = {u'ą':'a', u'ć':'c', u'ę':'e', u'ł':'l', u'ń':'n', u'ó':'o', u'ś':'s', u'ź':'z', u'ż':'z', 
               u'Ą':'A', u'Ć':'C', u'Ę':'E', u'Ł':'L', u'Ń':'N', u'Ó':'O', u'Ś':'S', u'Ź':'Z', u'Ż':'Z', 
